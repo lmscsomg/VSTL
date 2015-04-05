09f3be7d95465e91b251b2ae7b662c9ee31c4142
lms <lmscsomg@gmail.com>
2015-04-05 20:15:03 +0800

    add readme

---
+++ v_type_traits.h
@@ -0,0 +1,14 @@
+//
+//  v_type_traits.h
+//  VSTL
+//
+//  Created by lms on 4/5/15.
+//  Copyright (c) 2015 lms. All rights reserved.
+//
+
+#ifndef __VSTL__v_type_traits__
+#define __VSTL__v_type_traits__
+
+#include <stdio.h>
+
+#endif /* defined(__VSTL__v_type_traits__) */
